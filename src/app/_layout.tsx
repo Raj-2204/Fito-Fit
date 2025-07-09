50b117bf8d8aa8cce635f9558351b7596789eb14
Raj-2204 <rajveerbakhshi@gmail.com>
2025-07-09 19:01:56 -0300

    design is ready for login/signup page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,11 +2,12 @@ import "../global.css";
 import { Stack, Tabs } from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { Slot } from 'expo-router'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function Layout() {
   return (
-    <ClerkProvider>
+    <ClerkProvider tokenCache={tokenCache}>
       <Slot />
     </ClerkProvider>
   );
